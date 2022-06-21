35c45d25b1305f881d0eff268f0a2d0788a053bf
trenner76 <106338690+trenner76@users.noreply.github.com>
2022-06-21 18:05:11 +0300

    Add files via upload

---
+++ new.js
@@ -0,0 +1,70 @@
+console.log('hello js') 
+let user_name = "item_1"
+console.log(user_name)
+let item_1 = 5
+console.log(user_name)
+console.log("item_1")
+console .log(item_1)
+let users_name = "item_2"
+console.log(users_name)
+let item_2 = 3
+console.log(item_2)
+let useres_name = "item_3"
+console.log(useres_name)
+let item_3 = item_1 + item_2
+console.log(item_3)
+console.log("item_3 =", item_3)
+let userec = "item_4"
+console.log(userec)
+let item_4 = "Yolochka";
+console.log(item_4);
+console.log(item_3 + item_4)
+console.log(item_3 )
+console.log("item_4=" + item_3, typeof("item_4=" + item_3))
+console.log(item_3 * item_4)
+let uSer = "item_5"
+console.log(uSer)
+let userus = "item_5"
+let item_5 = useres_name
+console.log(item_5)
+let userst = "item_6"
+console.log(userst)
+let usersts = "item_6_type"
+console.log(usersts)
+let item_6 = 15
+console.log(userst)
+console.log(item_6)
+let item_6_type = typeof(item_6)
+console.log(`item_6 == `+item_6, item_6_type == `+item_6_type`)
+console.log("item_6=" , item_6, typeof(item_6))
+console.log("item_6==", item_6, "item_6_type==", item_6_type)
+let yser = "item_7"
+console.log(yser)
+let item_7 = ("item_6=" + item_6, typeof("item_6"))
+console.log(item_7)
+let ysers = "item_7_type"
+console.log(ysers)
+let item_7_type = typeof(item_7)
+console.log(item_7_type)
+console.log("item_7== " , item_7, "item_7_type == " , item_7_type)
+let age_1 = 20
+let age_2 = 18
+let age_3 = 60
+
+    if(age_1 < age_2) {
+        console.log(`You don\t have access cause your age is ` + age_1 + `. It\s less then` + age_2 +`.`);
+
+    } else {if(age_1 >= age_2 && age_1 < age_3) {
+        console.log(`Welcome!`);
+    }  if(age_1 > age_3) {
+        console.log(`Keep calm and look Culture channel!`);
+    } else {
+        console.log(`Technical work`)
+
+    }}
+    
+     
+    
+
+
+
