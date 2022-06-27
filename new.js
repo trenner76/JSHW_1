89d420f52abe85a6fbc29d5b2202b855ff6a8683
trenner76 <106338690+trenner76@users.noreply.github.com>
2022-06-27 19:10:27 +0300

    Add files via upload

--- new.js
+++ new.js
@@ -62,6 +62,34 @@ let age_3 = 60
         console.log(`Technical work`)
 
     }}
+    const age2 = 18;
+    const age3 = 60;
+
+const checkAge = function(age) {
+   {if(age && !isNaN(age)) {
+  if (age < age2) {
+    console.log("You don`t have access cause your age is `" + age +" `it`s less then");
+  } else if (age >= age2 && age < age3) {
+ console.log("Welcome !");
+  } else if (age > age3) {
+ console.log("`Keep calm and look Culture channel`");
+  } else {
+ console.log("`Technical work`");
+  }
+} else console.log(`Not an integer value`)
+}
+}
+  let a = promt(`Enter yuour age`)
+  checkAge(a)
+  console.log(a)
+
+
+
+
+
+
+
+    
     
      
     
